7fd75ba51f1d6c57b4a5359a350b071d1fcc2c63
Markus Ongyerth <ongy@ongy.net>
2017-07-01 21:12:07 +0200

    Add OngyPos and OngyRect, with their respective parsers for dzen

--- src/main.rs
+++ src/main.rs
@@ -60,7 +60,31 @@ impl<G, C> Renderable<G, C> for Colored<G, C> {
 }
 
 struct OngyStr(String);
+struct OngyPos(f64);
 struct Separator;
+
+struct OngyRect {
+    width: f64,
+    height: f64,
+}
+
+impl<G, C> Renderable<G, C> for OngyRect
+    where C: graphics::character::CharacterCache,
+          G: graphics::Graphics<Texture = <C as graphics::character::CharacterCache>::Texture> {
+
+    fn get_size(&self, _: &mut C, _: u32) -> f64 {
+        return self.width;
+    }
+
+    fn do_render(&self, g: &mut G, height: u32,
+                 trans: &graphics::math::Matrix2d, _: &mut C,
+                 c: graphics::types::Color) -> f64 {
+        // This will be in pixels for now. Percent will probably go into another type
+        let dims = [0.0, height as f64 / 2.0 - self.height / 2.0, self.width, height as f64 / 2.0 + self.height / 2.0];
+        graphics::rectangle(c, dims, trans.trans(0f64, 0f64), g);
+        return self.width;
+    }
+}
 
 impl<G, C> Renderable<G, C> for OngyStr
     where C: graphics::character::CharacterCache,
@@ -94,6 +118,18 @@ impl<G, C> Renderable<G, C> for Separator
                        trans.trans(0f64, 0f64), g);
 
         return 1f64;
+    }
+}
+
+impl<G, C> Renderable<G, C> for OngyPos {
+
+    fn get_size(&self, _: &mut C, _: u32) -> f64
+    { return self.0; }
+
+    fn do_render(&self, _: &mut G, _: u32,
+                 _: &graphics::math::Matrix2d, _: &mut C,
+                 _: graphics::types::Color) -> f64 {
+        return self.0;
     }
 }
 
@@ -168,6 +204,46 @@ impl<G, C> DzenIter<G, C> {
     }
 }
 
+fn parse_pos(text: &str) -> Option<OngyPos> {
+    let x = match text.find(';') {
+            Some(x) => &text[..x],
+            None => text,
+        };
+
+    /* There's a few special values that we wan't to discard, but don't error
+     * when they appear.
+     * dzen has all of them starting with a '_'
+     */
+    if x.starts_with('_') {
+        /* This exactly removes the offset from having an element in the trait
+         * Renderable implementation
+         */
+        return Some(OngyPos(-4f64));
+    }
+
+    return match f64::from_str_radix(x, 10) {
+        Ok(val) => Some(OngyPos(val)),
+        Err(_) => None
+    }
+}
+
+fn parse_rect(text: &str) -> Option<OngyRect> {
+    match text.find('x') {
+        Some(i) => {
+            let w_str = &text[..i];
+            let h_str = &text[i + 1..];
+
+            //TODO: Remove unwrap()
+            let w = f64::from_str_radix(w_str, 10).unwrap();
+            let h = f64::from_str_radix(h_str, 10).unwrap();
+
+            return Some(OngyRect{width: w, height: h});
+        }
+
+        None => return None
+    }
+}
+
 fn find_str(text: &String) -> Option<(OngyStr, String)> {
     match text.find('^') {
         None => {
@@ -233,13 +309,26 @@ impl<G, C> Iterator for DzenIter<G, C>
             }
             None => {
                 if self.text.starts_with("^fg(") {
+                    if self.text.starts_with("^fg()") {
+                        self.text = String::from(&self.text.as_str()[5 .. ]);
+                        return Some(Box::new(OngyPos(-4.0)));
+                    }
                     return match self.text.find(')') {
                         Some(i) => {
                             match parse_color(&self.text.as_str()[4 .. i]) {
                                 Some(c) =>  {
-                                    let tmp = dzen_parse(&self.text.as_str()[i + 1 ..]);
-                                    self.text.clear();
-                                    return Some(Box::new(Colored{ color: c, elem: Box::new(tmp)}));
+                                     match self.text.as_str()[i + 1..].find("^fg(") {
+                                        Some (j) => {
+                                            let tmp = dzen_parse(&self.text.as_str()[i + 1 .. i + 1 + j]);
+                                            self.text = String::from(&self.text.as_str()[i + 1 + j ..]);
+                                            return Some(Box::new(Colored{ color: c, elem: Box::new(tmp)}));
+                                        }
+                                        None => {
+                                            let tmp = dzen_parse(&self.text.as_str()[i + 1 ..]);
+                                            self.text.clear();
+                                            return Some(Box::new(Colored{ color: c, elem: Box::new(tmp)}));
+                                        }
+                                    }
                                 }
                                 None => None
                             }
@@ -247,6 +336,79 @@ impl<G, C> Iterator for DzenIter<G, C>
                         None => {
                             std::io::stderr().write(b"Found '^fg(', but couldn't find closing parens!").unwrap();
                             return None;
+                        }
+                    }
+                }
+                if self.text.starts_with("^p(") {
+                    return match self.text.find(')') {
+                        Some(i) => {
+                            match parse_pos(&self.text.as_str()[3 .. i]) {
+                                Some(r) =>  {
+                                    self.text = String::from(&self.text.as_str()[i + 1 ..]);
+                                    return Some(Box::new(r));
+                                }
+                                None => None
+                                }
+                            }
+                        None => {
+                            std::io::stderr().write(b"Found '^fg(', but couldn't find closing parens!").unwrap();
+                            return None;
+                        }
+                    }
+                }
+                if self.text.starts_with("^r(") {
+                    return match self.text.find(')') {
+                        Some(i) => {
+                            match parse_rect(&self.text.as_str()[3 .. i]) {
+                                Some(r) =>  {
+                                    self.text = String::from(&self.text.as_str()[i + 1 ..]);
+                                    return Some(Box::new(r));
+                                }
+                                None => None
+                                }
+                            }
+                        None => {
+                            std::io::stderr().write(b"Found '^fg(', but couldn't find closing parens!").unwrap();
+                            return None;
+                        }
+                    }
+                }
+                if self.text.starts_with("^bg(") {
+                    std::io::stderr().write(b"Sorry, ongybar currently ignores background colours").unwrap();
+                    match self.text.find(')') {
+                        None => {
+                            std::io::stderr().write(b"Found ^bg(, but not closing parens").unwrap();
+                            return None;
+                        }
+                        Some(i) => {
+                            self.text = String::from(&self.text.as_str()[i + 1..]);
+                            return Some(Box::new(OngyPos(-4.0)));
+                        }
+                    }
+                }
+                if self.text.starts_with("^pa(") {
+                    std::io::stderr().write(b"Sorry, ongybar currently ignores background colours").unwrap();
+                    match self.text.find(')') {
+                        None => {
+                            std::io::stderr().write(b"Found ^pa(, but not closing parens").unwrap();
+                            return None;
+                        }
+                        Some(i) => {
+                            self.text = String::from(&self.text.as_str()[i + 1..]);
+                            return Some(Box::new(OngyPos(-4.0)));
+                        }
+                    }
+                }
+                if self.text.starts_with("^i(") {
+                    std::io::stderr().write(b"Sorry, ongybar currently ignores images").unwrap();
+                    match self.text.find(')') {
+                        None => {
+                            std::io::stderr().write(b"Found ^i(, but not closing parens").unwrap();
+                            return None;
+                        }
+                        Some(i) => {
+                            self.text = String::from(&self.text.as_str()[i + 1..]);
+                            return Some(Box::new(OngyPos(-2.0)));
                         }
                     }
                 }
